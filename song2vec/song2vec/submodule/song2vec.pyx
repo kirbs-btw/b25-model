88bcfab85d0e196749acc035c95db58a4a9edc2f
Bastian Lipka <lipka.bastian04@gmail.com>
2024-11-02 11:49:38 +0100

    song2vec progress

--- song2vec/song2vec/submodule/song2vec.pyx
+++ song2vec/song2vec/submodule/song2vec.pyx
@@ -16,9 +16,9 @@ cdef class Song2Vec:
     cdef int algorithm
     cdef int epochs
     cdef float learning_rate
-    cdef dict song_vectors
-    cdef dict context_vectors
+    cdef dict vector_map
     
+
     def __init__(self, list training_data, int vector_size=16, int window=5, int min_count=1, int workers=1, int algorithm=0, int epochs=5, float learning_rate=0.01):
         self.vector_size = vector_size
         self.window = window
@@ -27,5 +27,25 @@ cdef class Song2Vec:
         self.algorithm = algorithm
         self.epochs = epochs
         self.learning_rate = learning_rate
+        self.vector_map = self.__create_vec_map(training_data)
+
         
-        print("lfkjsklfj")
+    def __create_vec_map(self, list training_data) -> dict:
+        cdef list unique_entries = []
+        cdef dict vec_map = {}
+        cdef np.ndarray[np.float64_t, ndim=1] random_vector
+        cdef str text
+        cdef int i
+
+        # Flatten the nested list and add unique entries
+        for cluster in training_data:
+            for text in cluster:
+                if text not in unique_entries:
+                    unique_entries.append(text)
+
+        # Create a random vector for each unique entry
+        for entry in unique_entries:
+            random_vector = np.random.uniform(-1, 1, self.vector_size)
+            vec_map[entry] = random_vector
+
+        return vec_map

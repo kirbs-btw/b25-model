338ec38bca25b850c15ab1ea93136c9469f9cece
Bastian Lipka <lipka.bastian04@gmail.com>
2025-04-03 17:47:16 +0200

    started cboe implementation - not ready

--- entity2vec/models/cboe.py
+++ entity2vec/models/cboe.py
@@ -10,4 +10,24 @@ class Cboe(EntityModel):
         # ref:
         # cbow from gensim
         # https://github.com/piskvorky/gensim/blob/develop/gensim/models/word2vec.py
-        ...
+        for raw_entity_set in training_data:
+            # creating a list of the vectors
+
+            entity_vectors = []
+            entity_list = []
+            for e in raw_entity_set:
+                if not self.in_vector_map(e):
+                    continue
+                entity_vectors.append(self.get_vector(e))
+                entity_list.append(e)
+
+            # checking if entity set has meaning (len > 1)
+            n = len(entity_vectors)
+            if n < 2:
+                continue
+            n_minus_one = n - 1
+            # ...
+            # embedding lookup and average
+            # output prediction
+            # softmax function
+            # loss (cross-entropy)
